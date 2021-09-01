4894bec036b7efda429c13ec452c077132143696
Iva <84078385+Mistymos3@users.noreply.github.com>
2021-09-01 14:58:39 -0700

    tried to add a clear function...doesn't work..yet

--- js/index.js
+++ js/index.js
@@ -19,6 +19,7 @@ function doYellow() {
 
 function doBlue() {
     var dd1 = document.getElementById("d1");
+    
     dd1.style.backgroundColor = "pink";
 
     var ctxt = dd1.getContext("2d");
@@ -33,14 +34,30 @@ function doBlack() {
         black.style.backgroundColor = "black";
 }
 
+//third canvas element to gray
 function changeColor1() {
     var gray = document.getElementById("c2");
         gray.style.backgroundColor = "gray";
 }
-
+//add black square to gray
 function addShape() {
-    var square = document.getElementById("c2")
-        var cxt = square.getContext("2d");
+    var square = document.getElementById("c2");
+      
+    var cxt = square.getContext("2d");
         cxt.fillStyle = "black";
         cxt.fillRect(95,95, 45,45);
 }
+//turn canvas to completely pink
+function pink() {
+    var pink = document.getElementById("c2");
+        pink.style.backgroundColor = "pink";
+}
+
+
+// How to clear NEEDS WORK...BECAUSE IT DOESN'T
+// function clear() {
+//     var canvas = document.getElementById("c2");
+//     var context = canvas.getContext("2d");
+//         context.clearRect(0,0,canvas.width, canvas.height);
+//         canvas.style.backgroundColor = "white";
+// }
